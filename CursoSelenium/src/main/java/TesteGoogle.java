ef5af12f1677bbf531859e67bea31832ec27d341
Fernando <Fernando@dk105857-me>
2021-07-23 17:29:17 -0300

    tamanho e fechar browser

--- CursoSelenium/src/main/java/TesteGoogle.java
+++ CursoSelenium/src/main/java/TesteGoogle.java
@@ -1,3 +1,5 @@
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -12,9 +14,12 @@ public class TesteGoogle {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Fernando\\Documents\\chromedriver.exe");
 	//	WebDriver driver = new FirefoxDriver();
 		WebDriver driver = new ChromeDriver();
+	//	driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.manage().window().maximize();
 		driver.get("http://www.google.com");
 	//	System.out.println(driver.getTitle());
 		Assert.assertEquals("Google", driver.getTitle());
+		driver.quit();
 	}
 	
 }
